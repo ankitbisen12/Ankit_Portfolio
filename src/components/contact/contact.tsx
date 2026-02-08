6ecf52bed412151104bc5770a73a83f654995205
Ankit Bisen <ankitbisen751@gmail.com>
2026-02-08 17:21:37 +0530

    Changed Heading content of Navbar and fixed type definition of formInput

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
 
     const formData = watch();
 
-    const onSubmit = async (formInput: any) => {
+    const onSubmit = async (formInput: { name: string; email: string; message: string }) => {
         // console.log("Form input is ", formInput);
         setLoading(true);
 
@@ -67,91 +67,88 @@ const Contact = () => {
 
     return (
         <section className="bg-black flex items-center justify-center px-4 py-20" id='contact'>
-                <div className="max-w-4xl w-full">
-                    <SectionHeader title="Contact" subTitle="Get In Touch" />
-                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                        <div className="space-y-8 lg:col-span-1">
-                            <Card bgColor='bg-black/50' >
-                                <h3 className="text-xl font-semibold text-white mb-4">
-                                    Contact Info.
-                                </h3>
-                                <p className="text-md text-gray-400 leading-tight">
-                                    Feel free to reach out for collaborations.
-                                </p>
-
-                                <div className="py-4 space-y-2">
-                                    <span className="flex items-center gap-2 text-gray-300 text-sm text-gray-400 font-semibold">
-                                        <Mail size={18} className="text-purple-500" />
-                                        ankitbisen751@gmail.com
-                                    </span>
-                                    <span className="flex items-center gap-2 text-gray-300 text-sm text-gray-400 font-semibold">
-                                        <Linkedin size={18} className="text-purple-500" />
-                                        linkedin.in/ankit-bisen
-                                    </span>
-                                </div>
-                            </Card>
-
-                            <Card bgColor='bg-gradient-to-br from-[#2b1d3a] via-[#1b152a] to-[#0b0b12]'>
-                                <h3 className="text-xl font-semibold text-white mb-6 ">
-                                    Looking for a dev?
-                                </h3>
-                                <p className="text-sm text-gray-400 mb-6 leading-tight font-semibold">
-                                    I'm currently available for freelance projects and open to full-time opportunities.
-                                </p>
-                                <Button bgColor="border border-purple-600" textColor="text-white" content="Download Resume" icon={<Download size={18} />} onClick={downloadResume} />
-                            </Card>
-                        </div>
-
-                        <div className="bg-black/50 rounded-2xl p-6 border border-gray-900 lg:col-span-2">
+            <div className="max-w-4xl w-full">
+                <SectionHeader title="Contact" subTitle="Get In Touch" />
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                    <div className="space-y-8 lg:col-span-1">
+                        <Card bgColor='bg-black/50' >
                             <h3 className="text-xl font-semibold text-white mb-4">
-                                Drop a Message
+                                Contact Info.
                             </h3>
-                            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                                <div className="flex flex-col lg:flex-row gap-4">
-                                    <div className="w-full lg:w-1/2">
-                                        <label className="block text-gray-300 mb-2 font-semibold">
-                                            Name
-                                        </label>
-                                        <input
-                                            type="text"
-                                            {...register('name')}
-                                            className="w-full px-4 py-2 bg-black/50 text-md border border-gray-700 rounded-lg text-white placeholder-gray-600 focus:outline-none"
-                                            placeholder="Full Name"
-                                            autoComplete="off"
-                                        />
-                                    </div>
+                            <p className="text-md text-gray-400 leading-tight">
+                                Feel free to reach out for collaborations.
+                            </p>
 
-                                    <div className="w-full lg:w-1/2">
-                                        <label  className="block text-gray-300 mb-2 font-semibold">
-                                            Email
-                                        </label>
-                                        <input
-                                            type="email"
-                                            {...register('email')}
-                                            className="w-full px-4 py-2 bg-black/50 text-md border border-gray-700 rounded-lg text-white placeholder-gray-600 focus:outline-none"
-                                            placeholder="your.email@example.com"
-                                            autoComplete="off"
-                                        />
-                                    </div>
-                                </div>
+                            <div className="py-4 space-y-2">
+                                <span className="flex items-center gap-2 text-sm text-gray-400 font-semibold">
+                                    <Mail size={18} className="text-purple-500" />
+                                    ankitbisen751@gmail.com
+                                </span>
+                                <span className="flex items-center gap-2  text-sm text-gray-400 font-semibold">
+                                    <Linkedin size={18} className="text-purple-500" />
+                                    linkedin.in/ankit-bisen
+                                </span>
+                            </div>
+                        </Card>
 
-                                <div>
-                                    <label  className="block text-gray-300 mb-2">
-                                        Message
+                        <Card bgColor='bg-gradient-to-br from-[#2b1d3a] via-[#1b152a] to-[#0b0b12]'>
+                            <h3 className="text-xl font-semibold text-white mb-6 ">
+                                Looking for a dev?
+                            </h3>
+                            <p className="text-sm text-gray-400 mb-6 leading-tight font-semibold">
+                                {"I'm currently available for freelance projects and open to full-time opportunities."}
+                            </p>
+                            <Button bgColor="border border-purple-600" textColor="text-white" content="Download Resume" icon={<Download size={18} />} onClick={downloadResume} />
+                        </Card>
+                    </div>
+
+                    <div className="bg-black/50 rounded-2xl p-6 border border-gray-900 lg:col-span-2">
+                        <h3 className="text-xl font-semibold text-white mb-4">
+                            Drop a Message
+                        </h3>
+                        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                            <div className="flex flex-col lg:flex-row gap-4">
+                                <div className="w-full lg:w-1/2">
+                                    <label className="block text-gray-300 mb-2 font-semibold">
+                                        Name
                                     </label>
-                                    <textarea
-                                        {...register('message')}
-                                        rows={3}
+                                    <input
+                                        type="text"
+                                        {...register('name')}
                                         className="w-full px-4 py-2 bg-black/50 text-md border border-gray-700 rounded-lg text-white placeholder-gray-600 focus:outline-none"
-                                        placeholder="Your message here..."
-                                        autoComplete="off"
+                                        placeholder="Full Name"
                                     />
                                 </div>
-                                <button className="w-full px-2 lg:px-8 py-2 bg-purple-900 text-white border border-gray-800 shake-vertical font-medium rounded-lg cursor-pointer flex items-center justify-center gap-2" type="submit">Send Message {loading ? <Loader size={16} /> : <SendHorizonal size={16} />}</button>
-                            </form>
-                        </div>
+
+                                <div className="w-full lg:w-1/2">
+                                    <label className="block text-gray-300 mb-2 font-semibold">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        {...register('email')}
+                                        className="w-full px-4 py-2 bg-black/50 text-md border border-gray-700 rounded-lg text-white placeholder-gray-600 focus:outline-none"
+                                        placeholder="your.email@example.com"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <label className="block text-gray-300 mb-2">
+                                    Message
+                                </label>
+                                <textarea
+                                    {...register('message')}
+                                    rows={3}
+                                    className="w-full px-4 py-2 bg-black/50 text-md border border-gray-700 rounded-lg text-white placeholder-gray-600 focus:outline-none"
+                                    placeholder="Your message here..."
+                                />
+                            </div>
+                            <button className="w-full px-2 lg:px-8 py-2 bg-purple-900 text-white border border-gray-800 shake-vertical font-medium rounded-lg cursor-pointer flex items-center justify-center gap-2" type="submit" suppressHydrationWarning={true}>Send Message {loading ? <Loader size={16} /> : <SendHorizonal size={16} />}</button>
+                        </form>
                     </div>
                 </div>
+            </div>
 
         </section>
     );
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
 import React from "react";
-import { Logs } from 'lucide-react'
+import { Logs } from "lucide-react";
 import Sidebar from "./sidebar";
 import { useScrollToSection } from "@/customHooks/useScrollToSection";
 
@@ -11,28 +11,86 @@ const Navbar = () => {
   return (
     <React.Fragment>
       <nav className="w-full bg-black px-2 lg:px-6 py-4 border-b border-gray-900">
-        <div className="mx-auto flex  items-center justify-between">
+        <div className="mx-auto flex items-center justify-between">
           <div className="flex items-center cursor-pointer gap-2 lg:gap-4">
-            <Logs size={24} className="text-white" onClick={() => setOpen((prev) => !prev)} />
+            <Logs
+              size={24}
+              className="text-white hidden sm:block cursor-pointer"
+              onClick={() => setOpen((prev) => !prev)}
+            />
             <div className="leading-tight">
-              <p className="text-white font-semibold text-2xl">Ankit Bisen
-                <span className="text-purple-500 font-semibold text-4xl ">{" "}.</span>
+              <p className="text-white font-semibold text-2xl ">
+                <span className="text-purple-600">{">_ ["}</span>
+                <span>sudo</span>
+                <span className="text-purple-600">{": :Ankit]"}</span>
+                <span className="text-purple-500 font-normal text-4xl ">
+                  {" "}
+                  .
+                </span>
               </p>
             </div>
           </div>
-          <a
-            href="/assets/resume.pdf"
-            download
-            className="border border-purple-600 px-6 py-2 text-sm font-semibold text-purple-500 shake-vertical inline-block"
-          >
-            RESUME
-          </a>
+
+          <div className="flex items-center gap-4">
+            <a
+              href="/assets/resume.pdf"
+              download
+              className="hidden sm:inline-block border border-purple-600 px-6 py-2 text-sm font-semibold text-purple-500 shake-vertical"
+            >
+              RESUME
+            </a>
+            <Logs
+              size={24}
+              className="inline-block md:hidden text-white cursor-pointer"
+              onClick={() => setOpen((prev) => !prev)}
+            />
+          </div>
         </div>
       </nav>
-      <Sidebar open={open} setOpen={setOpen} scrollToSection={scrollToSection} />
+
+      <Sidebar
+        open={open}
+        setOpen={setOpen}
+        scrollToSection={scrollToSection}
+      />
     </React.Fragment>
   );
 };
 
 export default Navbar;
 
+// import React from "react";
+// import { Logs } from 'lucide-react'
+// import Sidebar from "./sidebar";
+// import { useScrollToSection } from "@/customHooks/useScrollToSection";
+
+// const Navbar = () => {
+//   const { open, setOpen, scrollToSection } = useScrollToSection();
+
+//   return (
+//     <React.Fragment>
+//       <nav className="w-full bg-black px-2 lg:px-6 py-4 border-b border-gray-900">
+//         <div className="mx-auto flex  items-center justify-between">
+//           <div className="flex items-center cursor-pointer gap-2 lg:gap-4">
+//             <Logs size={24} className="text-white" onClick={() => setOpen((prev) => !prev)} />
+//             <div className="leading-tight">
+//               <p className="text-white font-semibold text-2xl">{">_[sudo:: Ankit]"}
+//                 <span className="text-purple-500 font-semibold text-4xl ">{" "}.</span>
+//               </p>
+//             </div>
+//           </div>
+//           <a
+//             href="/assets/resume.pdf"
+//             download
+//             className="border border-purple-600 px-6 py-2 text-sm font-semibold text-purple-500 shake-vertical inline-block"
+//           >
+//             RESUME
+//           </a>
+//         </div>
+//       </nav>
+//       <Sidebar open={open} setOpen={setOpen} scrollToSection={scrollToSection} />
+//     </React.Fragment>
+//   );
+// };
+
+// export default Navbar;
