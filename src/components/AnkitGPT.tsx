9e0a185b8bbaf590533e9b9fa8bf0f6dbcd192c5
Ankit Bisen <ankitbisen751@gmail.com>
2026-09-14 02:48:20 +0530

    Changed http://localhost:3000/api/chat to /api/chat

--- src/components/AnkitGPT.tsx
+++ src/components/AnkitGPT.tsx
@@ -104,7 +104,7 @@ export default function AnkitGPT() {
       setBusy(true);
 
       try {
-        const res = await fetch("http://localhost:3000/api/chat", {
+        const res = await fetch("/api/chat", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ messages: history }),
